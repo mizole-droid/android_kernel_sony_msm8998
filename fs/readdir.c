0d4a311588c9ff3ec9fb821354d1f203875b90bf
m1zole <68125875+m1zole@users.noreply.github.com>
2025-08-30 15:22:19 +0900

    susfs 1.3.8
    Change-Id: Idd8240b7a6267f250f62cfcfe4a8720fa5d3dba0

--- fs/readdir.c
+++ fs/readdir.c
@@ -21,6 +21,10 @@
 
 #include <asm/uaccess.h>
 
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs.h>
+#endif
+
 int iterate_dir(struct file *file, struct dir_context *ctx)
 {
 	struct inode *inode = file_inode(file);
@@ -288,6 +292,11 @@ static int filldir64(struct dir_context *ctx, const char *name, int namlen,
 		return -EINVAL;
 	dirent = buf->previous;
 	if (dirent) {
+#ifdef CONFIG_KSU_SUSFS_SUS_PATH
+		if (susfs_sus_ino_for_filldir64(ino)) {
+			return 0;
+		}
+#endif
 		if (__put_user(offset, &dirent->d_off))
 			goto efault;
 	}
